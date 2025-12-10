7003308da67e54bc8425fc4320cc0442717671a9
stonedjjh <danjim82@gmail.com>
2025-12-10 07:54:02 -0500

    Trabajando con Scope de variable

---
+++ Scope/scope_lab.js
@@ -0,0 +1,27 @@
+// Ámbito global
+var globalVar = "Soy una variable global";
+let globalLet = "También soy global, pero con ámbito de let";
+const globalConst = "Soy una constante global";
+
+
+{
+// Ámbito de bloque
+var blockVar = "Soy un var con ámbito de bloque";
+let blockLet = "Soy un let con ámbito de bloque";
+const blockConst = "Soy un const con ámbito de bloque";
+}
+
+//Block Scope
+//console.log(blockVar);
+console.log(blockLet);
+
+function show(){
+    var functionVar = "Soy una var con alcance de bloque";
+    let functionLet = "Soy un let con alcance de bloque";
+    const functionConst = "Soy un const con alcance de bloque";
+    }
+    show();
+    
+    console.log(functionVar); // Lanza ReferenceError
+    console.log(functionLet); // Lanza ReferenceError
+    console.log(functionConst); // Lanza ReferenceError
